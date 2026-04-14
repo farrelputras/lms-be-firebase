34f5d40025d86443ed1c318090852d0de9aeb166
farrelputras <fernandiofarrel@gmail.com>
2026-04-14 11:08:58 +0700

    chore: open cors access for public

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,7 +22,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN?.split(",") ?? ["http://localhost:3000"],
+    // origin: process.env.CORS_ORIGIN?.split(",") ?? ["http://localhost:3000"],
+    origin: "*",
   })
 );
 app.use(express.json());
